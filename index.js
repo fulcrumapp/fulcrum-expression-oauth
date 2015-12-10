a1bdbd4ec38463257c4211dfe352879df9d13906
Zac McCormick <zac.mccormick@gmail.com>
2015-12-10 13:29:32 -0500

    Use a simpler lib

--- index.js
+++ index.js
@@ -1,72 +1,17 @@
-var oauthSignature = require('oauth-signature');
-var merge = require('merge');
-
-function timestamp() {
-  return ~~((+new Date()) / 1000);
-}
-
-function nonce() {
-  for (var o = ''; o.length < 32;) {
-    o += '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'[Math.floor(Math.random() * 61)];
-  }
-  return o;
-}
+var ohauth = require('ohauth');
 
 function oauthRequest(options, callback) {
-  var requiredParams = ['url', 'consumerSecret', 'tokenSecret'];
-  var missingParams = [];
-
-  requiredParams.forEach(function (requiredParam) {
-    if (!(requiredParam in options)) {
-      missingParams.push(requiredParam);
-    }
+  var auth = ohauth.headerGenerator({
+    consumer_key: options.consumerSecret,
+    consumer_secret: options.tokenSecret
   });
 
-  var method = (options.method || 'GET').toUpperCase();
-  var url = options.url;
-  var consumerSecret = options.consumerSecret;
-  var tokenSecret = options.tokenSecret;
+  options.headers = options.headers || {};
+  options.headers['Authorization'] = auth(options.method || 'GET', options.url, options.qs || {});
 
-  if (missingParams.length > 0) {
-    callback('Missing required parameter(s): ' + missingParams.join(', '));
-    return;
-  }
+  var request = (typeof REQUEST === 'undefined' ? require('request') : REQUEST);
 
-  var parameters = {
-    oauth_consumer_key: consumerSecret,
-    oauth_token: tokenSecret,
-    oauth_nonce: nonce(),
-    oauth_timestamp: timestamp(),
-    oauth_signature_method: 'HMAC-SHA1',
-    oauth_version: '1.0'
-  };
-
-  if (options.qs) {
-    merge(parameters, options.qs);
-  }
-
-  var signature = oauthSignature.generate(method, url, parameters, consumerSecret, tokenSecret, { encodeSignature: true});
-
-  var oauthHeader = 'OAuth oauth_consumer_key="' + consumerSecret + '",oauth_nonce="' + parameters.oauth_nonce + '",oauth_signature_method="' + parameters.oauth_signature_method + '",oauth_timestamp="' + parameters.oauth_timestamp + '",oauth_version="' + parameters.oauth_version + '",oauth_signature="' + signature + '"';
-  var headers = {
-    'Authorization': oauthHeader
-  };
-
-  var requestOptions = {
-    url: url,
-    method: method,
-    headers: headers
-  };
-
-  if (options.qs) {
-    requestOptions.qs = options.qs;
-  }
-
-  // r = REQUEST;
-  r = require('request');
-
-  console.log(requestOptions)
-  r(requestOptions, callback);
+  request(options, callback);
 }
 
 module.exports = oauthRequest;
